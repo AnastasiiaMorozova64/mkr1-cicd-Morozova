b457210ca19a4aaac1a699379b9ef633fccdf245
AnastasiiaMorozova64 <prettyanastasia64@gmail.com>
2025-03-28 16:09:12 +0200

    add nation class

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def main():
+class Nation:
+    def __init__(self, title, territory_size, citizen_count):
+        self.title = title
+        self.territory_size = float(territory_size)
+        self.citizen_count = int(citizen_count)
+
+def execute():
     print("its main")
 
 if __name__ == "__main__":
-    main()
+    execute()
